f026775491a5479013415d50d8983a012680729e
Noah Holcombe <nholcom2@students.kennesaw.edu>
2018-05-30 13:07:05 -0400

    first commit

---
+++ UPS.SpaceX.Core/DeskDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UPS.SpaceX.Core
+{
+    public class DeskDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
